b137259a609a45b24de15356d42d6c25bcb20092
suleseid <122879276+suleseid@users.noreply.github.com>
2024-04-29 00:33:25 -0700

    Updated Project

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = requiure("path");
+const path = require("path");
 const fs =require("fs");
 const app = express();
 
@@ -12,6 +12,28 @@ app.get("/", (req, res) => {
   app.get("/support", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "support.html"));
   });
+
+  app.get("/submitform", (req, res) => {
+    // Notice that the data from the form is in the request query object.
+    // The information is in the form of key-value pairs with the key
+    // being the name of the input field and the value being the value of the input field.
+    console.log(req.query);
+  
+    // Let's save these values to a file and then send a response to the user.
+    fs.appendFile("formdata.txt", JSON.stringify(req.query) + '\n', (err) => {
+      if (err) {
+        console.error("Error writing to file", err);
+        res.status(500).send("An error occurred while processing your form.");
+        return;
+      }
+      console.log("The data was appended to file!");
+  
+      // Send a response back to the user.
+      res.send("Thank you for submitting the form");
+    });
+  });
+
+
 
   const PORT = 3000;
 app.listen(PORT, () => {
